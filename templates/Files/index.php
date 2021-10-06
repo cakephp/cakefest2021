fdbd9c51e8b068789f56f9dded1d5bb89b641cef
Jorge González <steinkel@gmail.com>
2021-10-06 12:54:29 +0100

    add SimplePaginator

--- templates/Files/index.php
+++ templates/Files/index.php
@@ -45,12 +45,9 @@
     </div>
     <div class="paginator">
         <ul class="pagination">
-            <?= $this->Paginator->first('<< ' . __('first')) ?>
             <?= $this->Paginator->prev('< ' . __('previous')) ?>
-            <?= $this->Paginator->numbers() ?>
             <?= $this->Paginator->next(__('next') . ' >') ?>
-            <?= $this->Paginator->last(__('last') . ' >>') ?>
         </ul>
-        <p><?= $this->Paginator->counter(__('Page {{page}} of {{pages}}, showing {{current}} record(s) out of {{count}} total')) ?></p>
+        <p><?= $this->Paginator->counter(__('Page {{page}}')) ?></p>
     </div>
 </div>
